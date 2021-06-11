5440a7fab3cd33691dcff3bd0cc9896264450b59
Thomas Marchand <me@thomas-marchand.com>
2021-06-11 21:30:53 +0200

    add programs

--- home.nix
+++ home.nix
@@ -4,31 +4,33 @@
   programs.home-manager.enable = true;
   home.packages = with pkgs; [ aria2 neofetch gotop speedtest-cli ];
 
-  zsh = {
-    enable = true;
-    initExtraFirst = ''
-      [ ! -d "$HOME/.zsh/fsh/" ] && mkdir $HOME/.zsh/fsh/
-      export FAST_WORK_DIR=$HOME/.zsh/fsh/;
-    '';
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.6.4";
-          sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
-        };
-      }
-      {
-        name = "fast-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zdharma";
-          repo = "fast-syntax-highlighting";
-          rev = "v1.55";
-          sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
-        };
-      }
-    ];
+  programs = {
+    zsh = {
+      enable = true;
+      initExtraFirst = ''
+        [ ! -d "$HOME/.zsh/fsh/" ] && mkdir $HOME/.zsh/fsh/
+        export FAST_WORK_DIR=$HOME/.zsh/fsh/;
+      '';
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.6.4";
+            sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
+          };
+        }
+        {
+          name = "fast-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zdharma";
+            repo = "fast-syntax-highlighting";
+            rev = "v1.55";
+            sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
+          };
+        }
+      ];
+    };
   };
 }
