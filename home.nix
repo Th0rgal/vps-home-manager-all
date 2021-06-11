0508ec6f9c89be84134b18683b88ceb9a56f02ae
Thomas Marchand <me@thomas-marchand.com>
2021-06-11 21:36:34 +0200

    add rtorrent

--- home.nix
+++ home.nix
@@ -2,5 +2,5 @@
 
 {
   programs.home-manager.enable = true;
-  home.packages = with pkgs; [ aria2 neofetch gotop speedtest-cli ];
+  home.packages = with pkgs; [ rtorrent aria2 neofetch gotop speedtest-cli ];
 }
